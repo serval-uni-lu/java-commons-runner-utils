59f3e667cb66a5fb344254a2fe6fcc5556d4fc73
kabinja <renaudrwemalika@gmail.com>
2021-08-13 09:22:51 +0200

    create a SonarQube compliant header license and update all the files with the new header

--- src/main/java/lu/uni/serval/commons/runner/utils/exception/BrokerException.java
+++ src/main/java/lu/uni/serval/commons/runner/utils/exception/BrokerException.java
@@ -4,9 +4,9 @@ package lu.uni.serval.commons.runner.utils.exception;
  * #%L
  * Runner Utils
  * %%
- * Copyright (C) 2021 University of Luxembourg, Renaud RWEMALIKA
+ * Copyright (C) 2021 University of Luxembourg
  * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
